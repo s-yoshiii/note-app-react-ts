e96a29a42fdf53d9fec0a4924fce39d0138893c1
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-04-16 16:25:08 +0900

    新しいノートを追加する関数を作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
+import { useState } from "react";
 import Main from "./components/Main";
 import Sidebar from "./components/Sidebar";
+type Note = {
+  id: number;
+  title: string;
+  content: string;
+  modDate: number;
+};
 
 function App() {
+  const [notes, setNotes] = useState<Note[]>([]);
+  const onAddNote = () => {
+    console.log("新しいノートが追加されました。");
+    const newNote: Note = {
+      id: 1,
+      title: "新しいノート",
+      content: "新しいノートの内容",
+      modDate: Date.now(),
+    };
+    setNotes([...notes, newNote]);
+    console.log(notes);
+  };
   return (
     <div className="block md:flex overflow-hidden bg-white rounded-lg">
-      <Sidebar />
+      <Sidebar onAddNote={onAddNote} />
       <Main />
     </div>
   );
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { FC } from "react";
+type Props = {
+  onAddNote: () => void;
+};
 
-const Sidebar = () => {
+const Sidebar: FC<Props> = ({ onAddNote }) => {
   return (
     <div className="block md:flex md:flex-shrink-0">
-      <div className="flex flex-col md:w-64">
+      <div className="flex flex-col md:w-62">
         <div className="flex flex-col flex-grow pt-5 overflow-y-auto bg-white border-r border-gray-50">
           <div className="flex flex-row justify-between items-center flex-shrink-0 px-4">
             <h2 className="block p-2 text-xl font-medium tracking-tighter text-gray-900">
               Note
             </h2>
             <div className="px-4">
-              <button className="px-5 py-2 lg:px-3 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-slate-700 lg:px-10 rounded-md hover:bg-slate-950">
+              <button
+                className="px-5 py-2 lg:px-3 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-slate-700 lg:px-10 rounded-md hover:bg-slate-950"
+                onClick={onAddNote}
+              >
                 追加
               </button>
             </div>
@@ -18,110 +24,40 @@ const Sidebar = () => {
           <div className="flex flex-col flex-grow px-4 mt-5">
             <nav className="flex-1 space-y-1 bg-white">
               <ul>
-                <li className="flex flex-col px-4 py-2 mt-1 text-base transition duration-500 cursor-pointer ease-in-out transform rounded-lg bg-gray-50 focus:shadow-outline focus:shadow-outline hover:bg-gray-100">
-                  <div className="inline-flex items-center w-full text-gray-900">
-                    <svg
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="1.5"
-                      className="w-4 h-4"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                      aria-hidden="true"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
-                      ></path>
-                    </svg>
-                    <span className="ml-4">タイトル</span>
+                <li className="flex flex-col p-4 mt-2 text-base transition duration-500 cursor-pointer ease-in-out transform rounded-lg bg-gray-50 focus:shadow-outline focus:shadow-outline hover:bg-gray-100">
+                  <div className="flex justify-between">
+                    <div className="flex-shrink">
+                      <div className="inline-flex items-center w-full text-gray-900">
+                        <svg
+                          fill="none"
+                          stroke="currentColor"
+                          strokeWidth="1.5"
+                          className="w-4 h-4"
+                          viewBox="0 0 24 24"
+                          xmlns="http://www.w3.org/2000/svg"
+                          aria-hidden="true"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+                          ></path>
+                        </svg>
+                        <span className="ml-4">タイトル</span>
+                      </div>
+                      <p className="leading-relaxed mt-2">
+                        本文が入ります。本文が入ります。
+                      </p>
+                      <div className="mt-4 text-gray-500 text-sm block w-full">
+                        最後の修正日:YYYY.MM.DD
+                      </div>
+                    </div>
+                    <div className="pl-4 flex-shrink-0">
+                      <button className="px-5 py-2 lg:px-3 text-base font-medium text-center text-slate-700 transition duration-500 ease-in-out transform bg-white border border-slate-700 lg:px-10 rounded-md hover:bg-slate-700 hover:text-white">
+                        削除
+                      </button>
+                    </div>
                   </div>
-                  <p className="leading-relaxed mt-2">
-                    本文が入ります。本文が入ります。
-                  </p>
-                  <div className="mt-4 text-gray-500 text-sm block w-full">
-                    最後の修正日:YYYY.MM.DD
-                  </div>
-                </li>
-              </ul>
-              <p className="px-4 pt-4 font-medium text-gray-900 uppercase">
-                Shortcuts
-              </p>
-              <ul>
-                <li>
-                  <a
-                    className="inline-flex items-center w-full px-4 py-2 mt-1 text-base text-gray-900 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline hover:bg-gray-50"
-                    href="#"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-4 h-4"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
-                      ></path>
-                    </svg>
-                    <span className="ml-4"> Tasks</span>
-                  </a>
-                </li>
-                <li>
-                  <a
-                    className="inline-flex items-center w-full px-4 py-2 mt-1 text-base text-gray-900 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline hover:bg-gray-50"
-                    href="#"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-4 h-4"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
-                      >
-                        {" "}
-                      </path>
-                    </svg>
-                    <span className="ml-4"> Reports</span>
-                  </a>
-                </li>
-                <li>
-                  <a
-                    className="inline-flex items-center w-full px-4 py-2 mt-1 text-base text-gray-900 transition duration-500 ease-in-out transform rounded-lg focus:shadow-outline hover:bg-gray-50"
-                    href="#"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-4 h-4"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z"
-                      ></path>
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z"
-                      ></path>
-                    </svg>
-                    <span className="ml-4"> Dashboard</span>
-                  </a>
                 </li>
               </ul>
             </nav>
