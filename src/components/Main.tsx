ee09b0eafd9a7e677b326fa85f5f3a56326909e0
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-04-22 00:15:46 +0900

    Mainコンポーネントを作成しよう

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,21 +2,53 @@ import React from "react";
 
 const Main = () => {
   return (
-    <div className="flex flex-col flex-1 overflow-hidden">
-      <main className="relative flex-1 overflow-y-auto focus:outline-none">
-        <div className="py-6">
-          <div className="px-4 mx-auto max-w-7xl sm:px-6 md:px-8">
-            <h1 className="text-lg text-neutral-600">
-              Here is where you put your stuff
-            </h1>
-          </div>
-          <div className="px-4 mx-auto max-w-7xl sm:px-6 md:px-8">
-            <div className="py-4">
-              <div className="rounded-lg bg-gray-50 h-96"></div>
+    <div className="flex flex-col flex-1 w-fit overflow-hidden">
+      <div className="py-8 px-4 mx-auto lg:py-16">
+        <h2 className="mb-4 text-xl font-bold text-gray-900 dark:text-white">
+          Add a new product
+        </h2>
+        <form action="#">
+          <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="title"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Ttitle
+              </label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Type product name"
+                required
+              />
+            </div>
+
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="description"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Description
+              </label>
+              <textarea
+                id="description"
+                rows="8"
+                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Your description here"
+              ></textarea>
             </div>
           </div>
-        </div>
-      </main>
+          <button
+            type="submit"
+            className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800"
+          >
+            Add product
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
